3c4f78aa79ea36d3aa386b29c24e2913c36ac807
oTwo <xiangwei3@staff.weibo.com>
2019-03-31 11:57:06 +0800

    less测试

---
+++ oTwo-cil/src/components/hh.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<el-checkbox v-model="checked">备选项</el-checkbox>
+	</div>
+</template>
+<script>
+import jquery from "jquery";
+import T from '@/assets/js/tool.js';
+export default {
+	name: 'HelloWorld',
+	data() {
+		return {
+			msg: 'HelloWorld',
+			checked: true
+		};
+	},
+	components: {
+		jquery
+	},
+	mounted() {
+		jquery.get().done(function (d) {
+			console.log(d);
+		})
+		console.log(T.DB);
+		console.log(jquery('body'));
+	}
+};
+
+</script>
+
+<!-- 用来污染公共css -->
+<style lang="less">
+</style>
+
+<!-- 这里用来写私有的css -->
+<style lang="less" scoped>
+@import '../assets/css/base.less';
+.hh{
+	font-size: 14px;
+}
+
+</style>
