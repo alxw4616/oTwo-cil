25d88f697682d20efa5216e5e94e80c6c327ea9c
oTwo <xiangwei3@staff.weibo.com>
2019-03-31 13:44:54 +0800

    lee 简明教程

--- oTwo-cil/src/router/index.js
+++ oTwo-cil/src/router/index.js
@@ -2,20 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import vw from '@/components/vw'
-
-Vue.use(VueRouter)
-
+import less from '@/components/less'
+Vue.use(VueRouter);
 export default new VueRouter({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/vw',
-      name: 'vw',
-      component: vw
-    }
-  ]
+	routes: [{
+		path: '/',
+		name: 'HelloWorld',
+		component: HelloWorld
+	}, {
+		path: '/vw',
+		name: 'vw',
+		component: vw
+	}, {
+		path: '/less',
+		name: 'less',
+		component: less
+	}]
 })
