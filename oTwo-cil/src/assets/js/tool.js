aa2f5408979519bfaea75e3849149028d2de65b5
oTwo <xiangwei3@staff.weibo.com>
2019-03-31 23:38:42 +0800

    深拷贝 extend 被从jquery中复制出来.

--- oTwo-cil/src/assets/js/tool.js
+++ oTwo-cil/src/assets/js/tool.js
@@ -2,6 +2,10 @@
  * 常用工具集
  * by oTwo 2019年3月31日 21:33:57
  * v 1.5 去 jquery.支持vue
+ * 		一些老的函数,现在被ES6语法取代. 这些函数将注释成ES6语法.
+ * 		jquery 3.3.1 现在有127K. 在项目中使用有些大.故tool.js 不在依赖jquery.
+ * 		深拷贝 extend 被从jquery中复制出来.
+ *
  */
 var tool = {};
 
@@ -25,6 +29,98 @@ var tool = {};
  */
 
 // ---- function --------------------------------------------------------------
+tool.isPlainObject = function( obj ) {
+	var proto, Ctor;
+
+	// Detect obvious negatives
+	// Use toString instead of jQuery.type to catch host objects
+	if ( !obj || toString.call( obj ) !== "[object Object]" ) {
+		return false;
+	}
+
+	proto = getProto( obj );
+
+	// Objects with no prototype (e.g., `Object.create( null )`) are plain
+	if ( !proto ) {
+		return true;
+	}
+
+	// Objects with prototype are plain iff they were constructed by a global Object function
+	Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+	return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
+}
+
+// 深拷贝
+tool.extend = function() {
+	var options, name, src, copy, copyIsArray, clone,
+		target = arguments[ 0 ] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
+
+	// Handle a deep copy situation
+	if ( typeof target === "boolean" ) {
+		deep = target;
+
+		// Skip the boolean and the target
+		target = arguments[ i ] || {};
+		i++;
+	}
+
+	// Handle case when target is a string or something (possible in deep copy)
+	if ( typeof target !== "object" && !isFunction( target ) ) {
+		target = {};
+	}
+
+	// Extend jQuery itself if only one argument is passed
+	if ( i === length ) {
+		target = this;
+		i--;
+	}
+
+	for ( ; i < length; i++ ) {
+
+		// Only deal with non-null/undefined values
+		if ( ( options = arguments[ i ] ) != null ) {
+
+			// Extend the base object
+			for ( name in options ) {
+				src = target[ name ];
+				copy = options[ name ];
+
+				// Prevent never-ending loop
+				if ( target === copy ) {
+					continue;
+				}
+
+				// Recurse if we're merging plain objects or arrays
+				if ( deep && copy && ( tool.isPlainObject( copy ) ||
+					( copyIsArray = Array.isArray( copy ) ) ) ) {
+
+					if ( copyIsArray ) {
+						copyIsArray = false;
+						clone = src && Array.isArray( src ) ? src : [];
+
+					} else {
+						clone = src && tool.isPlainObject( src ) ? src : {};
+					}
+
+					// Never move original objects, clone them
+					target[ name ] = tool.extend( deep, clone, copy );
+
+				// Don't bring in undefined values
+				} else if ( copy !== undefined ) {
+					target[ name ] = copy;
+				}
+			}
+		}
+	}
+
+	// Return the modified object
+	return target;
+};
+
+// 倒计时器
 /**
  * 倒计时器,该函数将返回一个countdown对象.
  * countdown对象拥有stop(),restart(),start(),done(callback),timeout()方法
@@ -98,6 +194,7 @@ tool.countdown = function(n, interval) {
 	countdown.timeout();
 	return countdown;
 };
+
 // 字符串填充
 // padStart （）用于头部补全，padEnd （）用于尾部补全。
 // 'ss'.padStart(3,'0')
